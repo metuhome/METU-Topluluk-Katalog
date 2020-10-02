65e76810185a25d89b773f4bc762eb58e89d43ba
Göktuğ Ekinci <goktugekinci@Goktug-MacBook-Pro.local>
2020-10-03 00:30:55 +0300

    a

--- update.py
+++ update.py
@@ -1,6 +1,9 @@
 import pandas as pd 
+import wget
 
 df = pd.read_csv("Topluluk Katalog Formu (Responses) - Form Responses 1.csv")
+
+df["Topluluk Bilgilendirme Dosyası"][0]
 
 names = list(df["Topluluk Adı"])
 
